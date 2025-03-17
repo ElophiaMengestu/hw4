39e68785f94f00d932a6170a50ea6d79a35a8c25
Elophia M <elophia.mengestu@kellogg.northwestern.edu>
2025-03-17 17:41:49 +0000

    Your commit message here

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "places", :action => "index" })
-  resources "entries"
-  resources "places"
-  resources "sessions"
-  resources "users"
+  root "places#index"
+
+  get "signup", to: "users#new"
+  post "signup", to: "users#create"
+
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+
+  resources :entries
+  resources :places
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
 end
+
