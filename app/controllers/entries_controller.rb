d4c25eb9620cd8018100eb2cd8cec7afcc105ef3
Elophia M <elophia.mengestu@kellogg.northwestern.edu>
2025-03-17 18:01:42 +0000

    before_action:require_login

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,7 @@
 class EntriesController < ApplicationController
-
+  before_action :require_login
   def new
+    @entry = Entry.new
   end
 
   def create
@@ -9,8 +10,11 @@ class EntriesController < ApplicationController
     @entry["description"] = params["description"]
     @entry["occurred_on"] = params["occurred_on"]
     @entry["place_id"] = params["place_id"]
+    @entry["user_id"] = session[:user_id]
     @entry.save
     redirect_to "/places/#{@entry["place_id"]}"
   end
-
+  def index
+    @entries = Entry.where(user_id: session[:user_id]) # ✅ Only show user's own entries
+  end
 end
